c79a448bda2a18bc5fad1c047a08a2451339661a
Jayanth <jayanth199622@gmail.com>
2023-02-19 11:57:41 +0530

    add ip1

--- lib/chatbot_fb/application.ex
+++ lib/chatbot_fb/application.ex
@@ -7,8 +7,9 @@ defmodule ChatbotFb.Application do
 
   @impl true
   def start(_type, _args) do
+    {:ok,{_,_,_,_,_,[ip]}} = :inet.gethostbyname('chatbot-test.gigalixirapp.com')
     children = [
-        {Plug.Cowboy,scheme: :http, plug: ChatbotFb.ChatRouter, options: [ip: "chatbot-test.gigalixirapp.com",port: 443]}
+        {Plug.Cowboy,scheme: :http, plug: ChatbotFb.ChatRouter, options: [ip: ip, port: 443]}
     ]
     # :observer.start()
     opts = [strategy: :one_for_one, name: ChatbotFb.Supervisor]
